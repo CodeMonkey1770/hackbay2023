cdfe514c1efb656428b64020b06d2fe87c545ace
Olli <oliver.niesslein@gmx.de>
2023-05-23 17:21:30 +0200

    comment: link to speech article

--- hackbay/src/app/speech.service.ts
+++ hackbay/src/app/speech.service.ts
@@ -9,6 +9,7 @@ export class SpeechService {
   }
 
   speakText(text: string) {
+    //https://developer.chrome.com/blog/web-apps-that-talk-introduction-to-the-speech-synthesis-api/
     let msg = new SpeechSynthesisUtterance(text);
     window.speechSynthesis.speak(msg);
   }
