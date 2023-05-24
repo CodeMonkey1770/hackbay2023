9ecfa0eb1a6bbbf6b2a672915157f79e08b3aac7
Olli <oliver.niesslein@gmx.de>
2023-05-24 12:44:20 +0200

    result component

--- hackbay/src/app/results-page/results-page.component.ts
+++ hackbay/src/app/results-page/results-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-results-page',
@@ -6,6 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./results-page.component.scss'],
   standalone: true
 })
-export class ResultsPageComponent {
+export class ResultsPageComponent implements OnInit {
+  animationFrames = [
+    ["IMG_0759", 0.3],
+    ["IMG_0760", 0.1],
+    ["IMG_0761", 0.4],
+    ["IMG_0763", 0.1],
+  ];
 
+  imageSrc: string = "";
+
+  ngOnInit() {
+    let index = 0;
+
+    const playAnimation = () => {
+      const currentPic = this.animationFrames[index][0];
+      const delay = this.animationFrames[index][1];
+
+      this.imageSrc = `/assets/pictures/HackBayGiraffe/${currentPic}.PNG`;
+
+      index = (index + 1) % this.animationFrames.length;
+
+      setTimeout(playAnimation, 1 * 200);
+    };
+
+    playAnimation();
+  }
 }
